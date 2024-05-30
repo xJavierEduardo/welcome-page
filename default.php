df67ec5b87bdbd928f313ac05c99d51614c17007
xJavierEduardo <70067907+xJavierEduardo@users.noreply.github.com>
2024-05-29 19:38:47 -0700

    default page php
    html y css in php file

---
+++ default.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Página por defecto</title>
+        <link rel="icon" type="image/x-icon" href="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=48,h=48,fit=crop,f=png/mP4NePxxEXHrL5RO/logo_alexia-A85rkXq1qXsWaoxr.png">
+        <meta charset="utf-8">
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+        <meta content="Página por defecto" name="description">
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <style>
+          img {
+  width: 200px; /* Cambia el ancho según lo que necesites */
+  display: block; /* Para que el margin: auto funcione */
+  margin: 0 auto; /* Centra horizontalmente */
+}
+
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f4f4f4;
+  color: #333;
+}
+.container {
+  width: 80%;
+  margin: 0 auto;
+  text-align: center;
+  padding-top: 100px;
+}
+h1 {
+  font-size: 3em;
+  margin-bottom: 20px;
+  color: #333;
+}
+p {
+  font-size: 1.2em;
+  margin-bottom: 40px;
+  color: #666;
+}
+.button {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  text-decoration: none;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+}
+.button:hover {
+  background-color: #0056b3;
+}
+
+        </style>
+    </head>
+    <body>
+    <div class="container">
+        <img
+        src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=329,h=117,fit=crop,trim=8.852459016393443;0;8.852459016393443;0/mP4NePxxEXHrL5RO/logo_alexia_portada-mv0l1Za9vVUzwKPq.png"
+        alt="VetAlexiaLogo"
+      ></img>
+      <h1>Pagina no encontrada</h1>
+      <p>
+        La URL solicitada no se encontró en este servidor.
+      </p>
+      <a href="https://vetalexia.com/" class="button">Inicio</a>
+    </div>
+    </body>
+</html>
